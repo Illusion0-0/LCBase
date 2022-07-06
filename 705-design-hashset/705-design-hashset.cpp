6ef52e426ee100af7668b0ef7053263e67736ca8
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2022-07-06 20:55:11 +0530

    Time: 91 ms (96.15%), Space: 41.7 MB (62.06%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,26 +1,26 @@
 class MyHashSet {
 public:
     vector<list<int>>v;
-    int sz = 100;
+    int sz = 1000;
     MyHashSet() {
         v.resize(sz);
     }
     
     void add(int key) {
         if(!contains(key))
-            v[key%100].push_back(key);
+            v[key%1000].push_back(key);
     }
     
     void remove(int key) {
         if(contains(key))
         {
-            auto i = find(v[key%100].begin(),v[key%100].end(),key);
-            v[key%100].erase(i);
+            auto i = find(v[key%1000].begin(),v[key%1000].end(),key);
+            v[key%1000].erase(i);
         }
     }
     
     bool contains(int key) {
-        if(find(v[key%100].begin(),v[key%100].end(),key)!=v[key%100].end()) return true;
+        if(find(v[key%1000].begin(),v[key%1000].end(),key)!=v[key%1000].end()) return true;
         return false;
     }
 };
