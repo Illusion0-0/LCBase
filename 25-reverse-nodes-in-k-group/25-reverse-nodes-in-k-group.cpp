e72b8c09f034d03d945d8d025854a3c63bf42bd8
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2022-07-08 01:37:55 +0530

    Time: 145 ms (5.67%), Space: 11.5 MB (30.14%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -10,23 +10,67 @@
  */
 class Solution {
 public:
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        vector<int>v;
-        int sz=0;
-        ListNode *temp=head,*temp2=head;
-        while(head!=NULL){
-            v.push_back(head->val);
+    ListNode *reverse(ListNode *head){
+        if(head==NULL)return NULL;
+        ListNode *p=NULL,*c=head,*n=head->next;
+        while(c!=NULL){
+            c->next=p;
+            p=c;
+            c=n;
+            if(n!=NULL)n=n->next;
+        }
+        return p;
+    }
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode *temp=head,*p=head,*s=head,*e=head;
+        if(left==right)return head;
+        for(int i=1;i<left;i++){
+            if(i==left-1)p=head;
             head=head->next;
-            sz++;
+            s=head;
         }
-        for(int i=0;i<sz;i+=k){
-            if(i+k<=sz)reverse(v.begin()+i,v.begin()+i+k);
+        for(int i=1;i<right-left+1;i++){
+            head=head->next;
+            e=head;
         }
-        for(int i:v){
-            // cout<<i<<" ";
-            temp->val=i;
+        ListNode *last=e->next,*kk=NULL;
+        e->next=NULL;
+        kk=s;
+        ListNode *val = reverse(s);
+        kk->next=last;
+        if(p!=kk)p->next=val;
+        else temp=val;
+        ListNode* t=temp;
+        while(temp!=NULL){
+            cout<<temp->val<<" ";
             temp=temp->next;
         }
+        cout<<endl;
+        return t;
+    }
+    
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        int r=1,i=1,l=k-1;
+        ListNode * temp=head,*temp2=head,*t=head,*p=head;
+        int sz=0;
+        while(temp!=NULL){
+            temp=temp->next;
+            sz++;
+        }
+        while(l--){
+            temp2=temp2->next;
+        }
+        while(r+k-1<=sz){
+            
+            if(i%k==0){
+                cout<<t->val<<" "<<r<<" "<<r+k-1<<endl;
+                ListNode * ptr = reverseBetween(t,r,r+k-1);
+                r+=k;
+                t=ptr;
+            }
+            i++;
+        }
         return temp2;
+        
     }
 };
