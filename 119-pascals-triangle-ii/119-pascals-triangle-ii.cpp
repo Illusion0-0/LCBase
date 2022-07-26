e544728df7d7794a6ae4080bb2dc45580d914b8b
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2022-07-26 21:55:15 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (91.78%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-      vector<vector<int>>vec;
-        int i=2,val=rowIndex;
-        vector<int>temp;
-        temp.push_back(1),vec.push_back(temp),temp.push_back(1);
-        vec.push_back(temp),temp.clear();
-        if(rowIndex==0)return {1};
-        else if(rowIndex==1)return {1,1};
-        else{
-        rowIndex--;
-        while(rowIndex--){
-            temp.push_back(1);
-            for(int j=0;j<vec[i-1].size()-1;j++){
-                temp.push_back(vec[i-1][j]+vec[i-1][j+1]);
-            }
-            temp.push_back(1);
-            vec.push_back(temp);
-            temp.clear();
-            i++;
-        }
-        return vec[val];  
-        }
+       vector<int>ans;
+       ans.push_back(1);
+       long long val=1;
+       for(int i=0;i<rowIndex;i++){
+           val=val*(rowIndex-i)/(i+1);
+           ans.push_back(val);
+       } 
+       return ans;
     }
 };
