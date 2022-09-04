2a3eb47c82ce9844c9637ed2a30a1e42e19ca13c
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2022-09-04 17:43:57 +0530

    Time: 65 ms (35.93%), Space: 13.5 MB (22.74%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
+    // BFS
     int dx[4] = {1,-1,0,0};
     int dy[4] = {0,0,1,-1};
-    
-    void dfs(int i, int j, int n, int m, vector<vector<char>>& grid ,vector<vector<bool>>&vis){
-        vis[i][j]=true;
-        for(int k=0;k<4;k++){
-            int ii=i+dx[k], jj=j+dy[k];
-            if(ii>=0 && jj>=0 && ii<n && jj<m && !vis[ii][jj] && grid[ii][jj]=='1')
-                dfs(ii,jj,n,m,grid,vis);
-        }
-    }
     
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size();
         vector<vector<bool>>vis(n, vector<bool>(m,false));
         int cnt=0;
+        queue<pair<int,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(!vis[i][j] && grid[i][j]=='1'){
-                    dfs(i,j,n,m,grid,vis);
+                    vis[i][j]=true;
+                    q.push({i,j});
                     cnt++;
+                    while(!q.empty()){
+                        pair<int,int>p = q.front();
+                        q.pop();
+                        for(int k=0;k<4;k++){
+                            int ii=p.first+dx[k], jj=p.second+dy[k];
+                            if(ii>=0 && jj>=0 && ii<n && jj<m && !vis[ii][jj] && grid[ii][jj]=='1'){
+                                vis[ii][jj]=true;
+                                q.push({ii,jj});
+                            }
+                                
+                        }
+                    }
                 }
             }
         }
