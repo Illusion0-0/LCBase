5f4d0acecd2432110d6b95358309b92d89ac9b21
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2022-12-02 18:37:33 +0530

    Time: 367 ms (23.94%), Space: 59.7 MB (67.09%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -3,12 +3,11 @@ public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int n=arr.size();
         int start=0, end=n-1;
-        while(start<=end){
+        while(start<end){
             int mid = start+(end-start)/2;
-            if((mid+1<n && arr[mid]>arr[mid+1]) && (mid-1>=0 && arr[mid]>arr[mid-1]))return mid;
-            else if((mid-1>=0 && arr[mid-1]<arr[mid])||(mid+1<n && arr[mid]<arr[mid+1]))start=mid+1;
-            else end=mid-1;
+            if(mid+1<n && arr[mid]<arr[mid+1])start=mid+1;
+            else end=mid;
         }
-        return arr.size()-1;
+        return start;
     }
 };
