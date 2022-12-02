0e987316ec2ed87a46c0412b54b85e86d688a3f0
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2022-12-02 16:55:36 +0530

    Time: 5 ms (28.17%), Space: 6.1 MB (27.49%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0)return false;
-        long double k = log10(n)/log10(2);
-        return (k==(int)k);
+        return (n<=0)?false:(log10(n)/log10(2)==(int)(log10(n)/log10(2)));
     }
 };
