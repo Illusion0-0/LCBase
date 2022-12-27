c547b21572673f07df71a80c38b354c2f41c269d
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2022-12-28 03:24:01 +0530

    Time: 328 ms (10.97%), Space: 58.9 MB (5.01%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
+    //--------------------------------Code for mod-----------------------------------------------
     const long mod = 1e9+7;
     int add(int a, int b) { return ((a % mod) + (b % mod)) % mod; }
     int sub(int a, int b) { return ((a % mod) - (b % mod) + mod) % mod; }
     int mul(int a, int b) { return ((a % mod) * (b % mod)) % mod; }
+    //--------------------------------Code for mod-----------------------------------------------
     
     int sumSubarrayMins(vector<int>& arr) {
+        //--------------------------------Code for histogram begins------------------------------
         int n = arr.size();
         vector<int>left(n,0),right(n,0);
         stack<int>s;
@@ -40,23 +43,25 @@ public:
             s.push(i);
             right[i]=rmin;
         }
+        //--------------------------------Code for histogram ends-------------------------------
+        
+        
+        //------------------------------Code for this problem begins----------------------------
         int ans=0;
         for(int i=0;i<n;i++){
             int r = right[i], l = left[i], v = arr[i];
             if(i-l==1 || i-l==(r-l)){
-                int a = sub(r,l);
-                int b = mul(a,v);
+                int a = sub(r,l), b = mul(a,v);
                 ans = add(ans , b);
             }
             else{
-                int a = sub(i,l);
-                int b = sub(r,i);
-                int k = mul(a,v);
-                int c = add(b,1);
+                int a = sub(i,l), b = sub(r,i);
+                int k = mul(a,v), c = add(b,1);
                 int d = mul(k,c);
                 ans = add(ans , d);
             }
         }
         return ans%mod;
+        //--------------------------------Code for this problem ends----------------------------
     }
 };
