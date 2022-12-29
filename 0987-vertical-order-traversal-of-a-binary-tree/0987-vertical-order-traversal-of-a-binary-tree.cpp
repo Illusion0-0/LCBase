65c48b2529faf924f1c91bd05833ae12bd740398
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2022-12-29 19:23:36 +0530

    Time: 4 ms (84.59%), Space: 15 MB (7.94%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,39 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-public:
-    int mi = INT_MAX, ma = INT_MIN;
-    void func(TreeNode *root, int l, int a, map<int,map<int,vector<int>>>&ans){
-        if(root==NULL)return;
-        mi = min(mi,l);
-        ma = max(ma,l);
-        ans[l][a].push_back(root->val);
-        func(root->left,l-1,a+1,ans);
-        func(root->right,l+1,a+1,ans);
+ public:
+  int leftMost = INT_MAX, rightMost = INT_MIN, Maxlvl = 0;
+  unordered_map<int, unordered_map<int, multiset<int>>> ans;
+  // 2-D Map to store nodes at every [col][row]
+  void func(TreeNode* root, int col, int row) {
+    if (root == NULL) return;
+    leftMost = min(leftMost, col);
+    rightMost = max(rightMost, col);
+    Maxlvl = max(Maxlvl, row);
+    ans[col][row].insert(root->val);
+    func(root->left, col - 1, row + 1);
+    func(root->right, col + 1, row + 1);
+  }
+  vector<vector<int>> verticalTraversal(TreeNode* root) {
+    vector<vector<int>> res;
+    func(root, 0, 0);
+    for (int i = leftMost; i <= rightMost; i++) {
+      vector<int> temp;
+      for (int j = 0; j <= Maxlvl; j++) {
+        for (int k : ans[i][j]) temp.push_back(k);
+      }
+      res.push_back(temp);
     }
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,vector<int>>>ans;
-        int l=0,a=0;
-        vector<vector<int>>res;
-        func(root,l,a,ans);
-        for(int i=mi;i<=ma;i++){
-            // vector<int>temp;
-            vector<int>temp;
-            for(auto j:ans[i]){
-                sort(j.second.begin(),j.second.end());
-                for(int k:j.second)temp.push_back(k);
-            }
-            res.push_back(temp);
-        }
-        return res;
-    }
+    return res;
+  }
 };
