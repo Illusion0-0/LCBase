51f024b991253fea022e8b544a6575d7d0ce7d00
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2022-12-31 03:30:48 +0530

    Time: 35 ms (96.59%), Space: 36.9 MB (45.61%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>ans;
+    
+    void func(TreeNode *root){
+        if(!root)return;
+        ans.push_back(root->val);
+        func(root->left);
+        func(root->right);
+    }
+    
+    bool findTarget(TreeNode* root, int k) {
+        func(root);
+        sort(ans.begin(),ans.end());
+        for(int i=0;i<ans.size();i++){
+            int ind = lower_bound(ans.begin(),ans.end(),k-ans[i])-ans.begin();
+            if(ind!=ans.size() && ind!=i && ans[ind]+ans[i]==k)return true;
+        }
+        return false;
+    }
+};
