d599100199d24f0d41df1c857f4f4c55b2553d2c
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2022-12-31 21:38:32 +0530

    Time: 62 ms (71.70%), Space: 37.4 MB (32.85%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -9,33 +9,30 @@
  */
 class Codec {
 public:
+
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if (root == nullptr) return "#";
-        return to_string(root->val)+","+serialize(root->left)+","+serialize(root->right);
+        if (root == NULL) return "#";
+        return to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right);
     }
 
-    // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        return mydeserialize(data);
+        if (data == "#") return NULL;
+        stringstream s(data);
+        return makeDeserialize(s);
     }
-    TreeNode* mydeserialize(string& data) {
-        if (data[0]=='#') {
-            if(data.size() > 1) data = data.substr(2);
-            return nullptr;
+    
+    TreeNode* makeDeserialize(stringstream& s) {
+        string str;
+        getline(s, str, ',');
+        if (str == "#") {
+            return NULL;
         } else {
-            TreeNode* node = new TreeNode(helper(data));
-            node->left = mydeserialize(data);
-            node->right = mydeserialize(data);
-            return node;
+            TreeNode* root = new TreeNode(stoi(str));
+            root->left = makeDeserialize(s);
+            root->right = makeDeserialize(s);
+            return root;
         }
-    }
-private:
-    int helper(string& data) {
-        int pos = data.find(',');
-        int val = stoi(data.substr(0,pos));
-        data = data.substr(pos+1);
-        return val;
     }
 };
 
