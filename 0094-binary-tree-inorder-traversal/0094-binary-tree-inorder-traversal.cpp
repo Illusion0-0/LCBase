3e0e2c4972349c6ca93707dc4ac6d90749f798cc
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-01-01 21:58:07 +0530

    Time: 3 ms (60.49%), Space: 8.2 MB (98.37%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,16 +11,43 @@
  */
 class Solution {
 public:
-    vector<int>ans;
-    void inorder(TreeNode* root){
-        if(root==NULL) return ;
-        inorder(root->left);
-        ans.push_back(root->val);
-        inorder(root->right);    
-    }
+// Space: O(n)
+//     vector<int>ans;
+//     void inorder(TreeNode* root){
+//         if(root==NULL) return ;
+//         inorder(root->left);
+//         ans.push_back(root->val);
+//         inorder(root->right);    
+//     }
     
-    vector<int> inorderTraversal(TreeNode* root) {
-        inorder(root);
-        return ans;
-    }
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         inorder(root);
+//         return ans;
+//     }
+// Space: O(1) : Morris Traversal
+       vector<int> inorderTraversal(TreeNode* root) {
+           vector<int>inorder;
+           while(root){
+              if(root->left==NULL){
+                  inorder.push_back(root->val);
+                  root = root->right; 
+              }
+              else{
+                  TreeNode *temp = root->left;
+                  while(temp->right && temp->right!=root){
+                      temp=temp->right;
+                  }
+                  if(temp->right==NULL){
+                      temp->right=root;
+                      root=root->left;
+                  }
+                  else{
+                      temp->right=NULL;
+                      inorder.push_back(root->val);
+                      root=root->right;
+                  }
+              }
+           }
+           return inorder;
+       }
 };
