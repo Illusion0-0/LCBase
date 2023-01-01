1ab7569f65d7d040daf51438b264b080328fd126
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-01-01 19:44:22 +0530

    Time: 35 ms (77.65%), Space: 23.4 MB (23.18%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,36 +10,17 @@
 
 class Solution {
 public:
-    vector<int>a,b;
+    TreeNode *ans=NULL;
     
-    bool fun1(TreeNode* root, TreeNode* p){
-        if(root==NULL)return false;
-        a.push_back(root->val);
-        if(root==p)return true;
-        if(fun1(root->left,p) || fun1(root->right,p))
-            return true;
-        a.pop_back();
-        return false;
-    }
-    
-    bool fun2(TreeNode* root, TreeNode* p){
-        if(root==NULL)return false;
-        b.push_back(root->val);
-        if(root==p)return true;
-        if(fun2(root->left,p) || fun2(root->right,p))
-            return true;
-        b.pop_back();
-        return false;
+    void helper(TreeNode* root, int p, int q){
+        if(!root)return;
+        if(p<=root->val && q>=root->val){ans=root;return;}
+        helper(root->left,p,q);
+        helper(root->right,p,q);
     }
     
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        fun1(root,p);
-        fun2(root,q);
-        int i;
-        for(i=0;i<min(a.size(),b.size());i++){
-            if(a[i]!=b[i])break;
-        }
-        TreeNode *ans = new TreeNode(a[i-1]);
+        helper(root, min(p->val,q->val), max(p->val,q->val));
         return ans;
     }
 };
