6438effece4bd56e7a7b47703e26a02f26867188
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-01-01 21:20:39 +0530

    Time: 34 ms (97.04%), Space: 37 MB (45.70%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,23 +9,63 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+class BSTIterator {
 public:
-    vector<int>ans;
-    
-    void func(TreeNode *root){
-        if(!root)return;
-        func(root->left);
-        ans.push_back(root->val);
-        func(root->right);
+    // Time: O(h)
+    // Space: O(h)
+    stack<TreeNode*>s;
+    bool rev=true;
+    void pushAllLeft(TreeNode *root){
+        while(root){
+            s.push(root);
+            if(rev)root=root->right;
+            else root=root->left;
+        }
     }
     
-    bool findTarget(TreeNode* root, int k) {
-        func(root);
-        sort(ans.begin(),ans.end());
-        for(int i=0;i<ans.size();i++){
-            int ind = lower_bound(ans.begin(),ans.end(),k-ans[i])-ans.begin();
-            if(ind!=ans.size() && ind!=i && ans[ind]+ans[i]==k)return true;
+    BSTIterator(TreeNode* root, bool r) {
+        rev=r;
+        pushAllLeft(root);
+    }
+    
+    int next() {
+        TreeNode *temp=NULL;
+        if(hasNext()){
+            temp=s.top();
+            s.pop();
+        }
+        if(!rev)
+            pushAllLeft(temp->right);
+        else 
+            pushAllLeft(temp->left);
+        return temp->val;
+    }
+    
+    bool hasNext() {
+        return !s.empty();
+    }
+};
+
+
+class Solution {
+public:
+    // unordered_set<int> s;
+    // bool findTarget(TreeNode* root, int k) {
+    //     if (!root) return false;
+    //     if (s.count(k - root->val)) return true;
+    //     s.insert(root->val);
+    //     return findTarget(root->left, k) || findTarget(root->right, k);
+    // }
+    bool findTarget(TreeNode* root, int k){
+        BSTIterator l(root, false);
+        BSTIterator r(root, true);
+        int i = l.next(), j = r.next();
+        while(i<j){
+            int sum = i+j;
+            if(sum==k)return true;
+            else if(sum<k)i=l.next();
+            else j=r.next();
         }
         return false;
     }
