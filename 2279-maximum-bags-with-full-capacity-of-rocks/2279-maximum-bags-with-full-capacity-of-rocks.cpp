64462237531127a0038df043e8738ea36aa56271
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-01-03 15:40:35 +0530

    Time: 667 ms (5.01%), Space: 89.6 MB (31.34%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
         int n = rocks.size(), ans = 0;
-        vector<int>diff;
+        priority_queue<int, vector<int>, greater<int>>pq;
         for(int i=0;i<n;i++){
             if(capacity[i]==rocks[i]) ans++;
-            else diff.push_back(capacity[i]-rocks[i]);
+            else pq.push(capacity[i]-rocks[i]);
         }
-        priority_queue<int, vector<int>, greater<int>>pq(diff.begin(),diff.end());
         while(!pq.empty()){
             if(additionalRocks && pq.top() && pq.top()<=additionalRocks){
                 ans++;
