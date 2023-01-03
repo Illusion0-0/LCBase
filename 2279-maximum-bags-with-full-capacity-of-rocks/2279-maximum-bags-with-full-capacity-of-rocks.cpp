19afaff5bec2b1428123e7d572963a5bfb086316
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-01-03 15:39:39 +0530

    Time: 474 ms (5.01%), Space: 91.7 MB (7.68%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -4,8 +4,8 @@ public:
         int n = rocks.size(), ans = 0;
         vector<int>diff;
         for(int i=0;i<n;i++){
-            diff.push_back(capacity[i]-rocks[i]);
             if(capacity[i]==rocks[i]) ans++;
+            else diff.push_back(capacity[i]-rocks[i]);
         }
         priority_queue<int, vector<int>, greater<int>>pq(diff.begin(),diff.end());
         while(!pq.empty()){
