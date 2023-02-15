826d462e3cfb7ea3adf7f2a0de1694ceaecf1b42
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-02-15 12:48:25 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (100.00%) - LeetHub

--- 2177-find-three-consecutive-integers-that-sum-to-a-given-number/2177-find-three-consecutive-integers-that-sum-to-a-given-number.cpp
+++ 2177-find-three-consecutive-integers-that-sum-to-a-given-number/2177-find-three-consecutive-integers-that-sum-to-a-given-number.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     vector<long long> sumOfThree(long long num) {
-        if((num-3)%3==0){
-            long long a = (num-3)/3;
-            return {a,a+1,a+2};
-        }
+        long long x = num / 3;
+        if(x * 3 == num)  return {x - 1, x, x + 1};
         return {};
     }
 };
