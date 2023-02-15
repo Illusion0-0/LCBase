31699ff55faa63d02480f597219c2432d76a7aef
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-02-15 15:54:44 +0530

    Time: 570 ms (36.57%), Space: 141 MB (23.35%) - LeetHub

---
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]=i;
+        }
+        for(auto i:operations){
+            m[i[1]]=m[i[0]];
+            nums[m[i[0]]]=i[1];
+        }
+        return nums;
+    }
+};
