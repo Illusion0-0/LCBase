0fef4789d1199c0f0af947291d71fbc3c44c34b9
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-02-21 16:01:28 +0530

    Time: 3 ms (87.71%), Space: 8.4 MB (49.55%) - LeetHub

--- 2079-watering-plants/2079-watering-plants.cpp
+++ 2079-watering-plants/2079-watering-plants.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     int wateringPlants(vector<int>& plants, int capacity) {
-        int sum=0, n = plants.size();        
-        vector<int>pre(n);
-        for(int i=0;i<n;i++){
-            sum+=plants[i];
-            pre[i]=sum;
-        }
-        int ans=0, extra=0;
-        while(true){
-            int c = capacity+extra;
-            int x = upper_bound(begin(pre),end(pre),c)-begin(pre);
-            if(x<n){
-                ans+=2*x;
-                extra = pre[x-1];
+        // int sum=0, n = plants.size();        
+        // vector<int>pre(n);
+        // for(int i=0;i<n;i++){
+        //     sum+=plants[i];
+        //     pre[i]=sum;
+        // }
+        // int ans=0, extra=0;
+        // while(true){
+        //     int c = capacity+extra;
+        //     int x = upper_bound(begin(pre),end(pre),c)-begin(pre);
+        //     if(x<n){
+        //         ans+=2*x;
+        //         extra = pre[x-1];
+        //     }
+        //     else{
+        //         ans+=x;
+        //         break;
+        //     }
+        // }
+        // return ans;
+        /* In 1 pass */
+        int ans = 0, cur = capacity;
+        for(int i = 0; i < plants.size(); i++) {
+            if(plants[i] > cur) {      
+                cur = capacity;
+                ans += 2*i;
             }
-            else{
-                ans+=x;
-                break;
-            }
+            ans++;               
+            cur -= plants[i];         
         }
         return ans;
     }
