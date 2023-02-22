0515bd73fcd80069711f69829ac8ee36f4ab5691
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-02-22 14:30:06 +0530

    Time: 113 ms (8.53%), Space: 26.2 MB (37.41%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -5,28 +5,18 @@ public:
         for(int i:nums)r+=i;
         while(l<=r){
             int mid = l+(r-l)/2;
-            // cout<<mid<<" ";
-            int flag=0, sum=0, day=0;
+            int day=0, sum=0;
             for(int i=0;i<n;i++){
                 if(nums[i]>mid)day=k+1;
                 if(sum+nums[i]<=mid) sum+=nums[i];
                 else{
                     sum=nums[i];
                     day++;
-                    // if(k-day==n-i){
-                    //     flag=1;
-                    //     break;
-                    // }
                 }
             }
-            if(day<k){
-                r=mid-1;
-            }
-            else{
-                l=mid+1;                
-            }
+            if(day<k) r=mid-1;
+            else l=mid+1;                
         }
-        // cout<<endl;
         return l;
     }
 };
