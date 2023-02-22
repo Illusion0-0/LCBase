33136f18e78ced1a92f5efc4e2c73dc918a85acb
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-02-22 14:29:10 +0530

    Time: 91 ms (16.19%), Space: 26.1 MB (37.41%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& nums, int k) {
-        int l=0,r=0;
-        for(int i:nums)l=max(l,i),r+=i;
+        int l=0, r=0, n=nums.size();
+        for(int i:nums)r+=i;
         while(l<=r){
             int mid = l+(r-l)/2;
-            int sum=0, div=1;
-            for(int i:nums){
-                if(sum+i<=mid) sum+=i;
+            // cout<<mid<<" ";
+            int flag=0, sum=0, day=0;
+            for(int i=0;i<n;i++){
+                if(nums[i]>mid)day=k+1;
+                if(sum+nums[i]<=mid) sum+=nums[i];
                 else{
-                    div++;
-                    sum=i;
+                    sum=nums[i];
+                    day++;
+                    // if(k-day==n-i){
+                    //     flag=1;
+                    //     break;
+                    // }
                 }
             }
-            if(div<=k)r=mid-1;
-            else l=mid+1;
+            if(day<k){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;                
+            }
         }
+        // cout<<endl;
         return l;
     }
 };
