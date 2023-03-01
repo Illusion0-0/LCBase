e0cab1e59b3ae1aaf616c9a84be80d1df9f8fee1
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-03-01 22:13:42 +0530

    Time: 135 ms (85.15%), Space: 65 MB (39.83%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,20 +1,24 @@
-vector<int>v(100001,0);
 class Solution {
 public:
-    unordered_map<int,int>m;
-    vector<int> sortArray(vector<int>& nums) {
-        vector<int>ans;
-        for(int i:nums){
-            int x = i+(5*10000);
-            v[x]=1;
-            m[x]++;
+    vector<int> sortArray(vector<int>& arr) {
+        int max = *max_element(arr.begin(), arr.end());
+        int min = *min_element(arr.begin(), arr.end());
+        int range = max - min + 1;
+
+        vector<int> count(range), output(arr.size());
+        for (int i = 0; i < arr.size(); i++)
+            count[arr[i] - min]++;
+
+        for (int i = 1; i < count.size(); i++)
+            count[i] += count[i - 1];
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            output[count[arr[i] - min] - 1] = arr[i];
+            count[arr[i] - min]--;
         }
-        for(int i=0;i<100001;i++){
-            if(v[i]==1)
-            for(int j=0;j<m[i];j++){
-                ans.push_back(i-(5*10000));
-            }
-        }
-        return ans;
+
+        for (int i = 0; i < arr.size(); i++)
+            arr[i] = output[i];
+        return arr;
     }
 };
