587e4d1d89007a7f3a227dd9db989324d52f62b1
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-03-10 14:52:09 +0530

    Time: 77 ms (86.06%), Space: 11.6 MB (83.70%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,4 +1,39 @@
+int dp[20][10005];
+
 class Solution {
+public:
+    int res=-1;
+    
+    int helper(vector<int>&arr, int ind, int sumLeft){
+        if(sumLeft<0)return INT_MAX-1;
+        if(ind==arr.size())
+            return (sumLeft==0)?0:INT_MAX-1;
+        
+        if(dp[ind][sumLeft]!=-1)
+            return dp[ind][sumLeft];
+        
+        bool ans = false;
+        int cnt = 0;
+        if(arr[ind]>sumLeft){
+            dp[ind][sumLeft] = cnt = 0 + helper(arr,ind+1,sumLeft);
+        }
+        else{
+            int take = 1 + helper(arr,ind,sumLeft-arr[ind]);
+            int notake = 0 + helper(arr,ind+1,sumLeft);
+            cnt = min(take,notake);
+        }
+        
+        return dp[ind][sumLeft]=res=cnt;
+    }
+    
+    int coinChange(vector<int>& coins, int amount) {
+        memset(dp,-1,sizeof(dp));
+        helper(coins,0,amount);
+        return (res==INT_MAX-1)?-1:res;
+    }
+};
+
+/*class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int>arr(amount+1,amount+1);
@@ -11,4 +46,4 @@ public:
         }
         return arr[amount]!=amount+1?arr[amount]:-1;
     }
-};
+};*/
