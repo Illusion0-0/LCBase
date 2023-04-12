85ff919a130d92a23dd4ce874c1ac9cca32f8508
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-04-12 10:44:18 +0530

    Time: 11 ms (18.43%), Space: 7.8 MB (85.65%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        path+='/';
+        int n = path.length();
+        vector<string>ans;
+        string temp="";
+        for(int i=1;i<n;i++){
+            if(path[i-1]=='/' && path[i]=='/')continue;
+            if(path[i]!='/'){
+                temp+=path[i];
+            }
+            else{
+                cout<<temp<<" ";
+                if(temp.size()==1 && temp[0]=='.'){temp="";continue;}
+                else if(temp.size()==2 && temp[0]==temp[1] && temp[0]=='.'){
+                    if(ans.size())ans.pop_back();
+                    temp="";
+                }
+                else{
+                    ans.push_back(temp);
+                    temp="";
+                }
+            }
+        }
+        string res="/";
+        for(string k:ans){
+            res+=k+'/';
+        }
+        if(res.size()>1)res.pop_back();
+        cout<<endl;
+        return res;
+    }
+};
