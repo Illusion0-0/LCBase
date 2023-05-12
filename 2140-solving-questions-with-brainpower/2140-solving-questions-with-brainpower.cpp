9ea3c75e76da614210c34b77001b23df4a4a5bc9
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-05-12 23:54:29 +0530

    Time: 397 ms (28.09%), Space: 121.2 MB (56.85%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,19 @@
+long long dp[200005];
+class Solution {
+public:
+    
+    long long rec(vector<vector<int>>& q, int ind){
+        if(ind>=q.size())return 0LL;
+        if(ind==q.size()-1)return q[ind][0];
+        if(dp[ind]!=0)return dp[ind];
+        long long take = q[ind][0] + rec(q,ind+q[ind][1]+1);
+        long long ntake = rec(q,ind+1);
+        return dp[ind] = max(take,ntake);
+    }
+    
+    long long mostPoints(vector<vector<int>>& q) {
+        int n = q.size();
+        fill(dp,dp+2*n+4,0);
+        return rec(q,0);
+    }
+};
