be14b086d588ab42356a0984dcc674cc218e7fd4
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-06-07 03:10:03 +0530

    Time: 25 ms (5.68%), Space: 8.7 MB (6.06%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,30 +1,12 @@
-vector<int>preAns(100005,-1);
-
 class Solution {
 public:
-    string decimalToBinary(int n)
-    {
-	    string s = bitset<64> (n).to_string();
-    	const auto loc1 = s.find('1');
-	    if(loc1 != string::npos)
-		    return s.substr(loc1);
-	    return "0";
-    }
-    
-    void solve(){
-        for(int i=0;i<=1e5;i++){
-            int one=0;
-            string s = decimalToBinary(i);
-            for(char c:s)if(c=='1')one++;
-            preAns[i]=one;
-        }
-    }
-    
     vector<int> countBits(int n) {
-        if(preAns[0]==-1)solve();
-        vector<int>ans(n+1,0);
+        vector<int>ans;
         for(int i=0;i<=n;i++){
-            ans[i]=preAns[i];
+            bitset<32>b = i;
+            int one=0;
+            for(int i=0;i<32;i++) if(b[i]) one++;
+            ans.push_back(one);
         }
         return ans;
     }
