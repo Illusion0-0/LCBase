fff6b2a5d72206be825915353c606480a3ab489b
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-06-08 10:31:41 +0530

    Time: 4 ms (38.14%), Space: 6.6 MB (8.09%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool isHappy(int n) {
         string s = to_string(n);
-        set<int>st;
+        unordered_map<string,int>m;
         while(true){
             int k = 0;
-            st.insert(stoi(s));
+            m[s]++;
             for(int i=0;i<s.size();i++) 
                 k+=(s[i]-'0')*(s[i]-'0');
             s = to_string(k);
             if(k==1)return true;
-            if(st.find(k)!=st.end())return false;
+            if(m[s])return false;
         }
         // cout<<endl;
         return false;
