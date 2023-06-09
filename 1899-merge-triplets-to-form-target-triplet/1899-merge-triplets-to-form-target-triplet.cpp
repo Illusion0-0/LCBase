c0890526ff2f5026267217d35c6f8cf0486520ef
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-06-09 11:41:03 +0530

    Time: 592 ms (37.26%), Space: 146.3 MB (85.33%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
-        int n = triplets.size();
-        int l = target[0], m = target[1], r = target[2];
-        int x,y,z; x=y=z=-1;
-        for(int i=0;i<n;i++){
+        int l = target[0], m = target[1], r = target[2], x,y,z; x=y=z=-1;
+        for(int i=0;i<triplets.size();i++){
             if(triplets[i][1]<=m && triplets[i][2]<=r && triplets[i][0]==l)x=i;
             if(triplets[i][1]==m && triplets[i][2]<=r && triplets[i][0]<=l)y=i;
             if(triplets[i][1]<=m && triplets[i][2]==r && triplets[i][0]<=l)z=i;
         }
-        if(x<0 || y<0 || z<0)return false;
-        return true;
+        return !(x<0 || y<0 || z<0);
     }
 };
