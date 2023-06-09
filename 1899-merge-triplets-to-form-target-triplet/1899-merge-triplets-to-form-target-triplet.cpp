1591f81567063bf7d2d8a6e19dc98b40c8397a0f
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-06-09 11:36:10 +0530

    Time: 549 ms (75.87%), Space: 146.4 MB (85.33%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -2,19 +2,12 @@ class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
         int n = triplets.size();
-        vector<vector<int>>mid=triplets, right=triplets;
-        for(auto &i:mid)   swap(i[0],i[1]);
-        for(auto &i:right) swap(i[0],i[2]);
         int l = target[0], m = target[1], r = target[2];
         int x,y,z; x=y=z=-1;
         for(int i=0;i<n;i++){
-            if(triplets[i][1]<=m && triplets[i][2]<=r && triplets[i][0]==l){x=i;break;}
-        }
-        for(int i=0;i<n;i++){
-            if(mid[i][1]<=l && mid[i][2]<=r && mid[i][0]==m){y=i;break;}
-        }
-        for(int i=0;i<n;i++){
-            if(right[i][1]<=m && right[i][2]<=l && right[i][0]==r){z=i;break;}
+            if(triplets[i][1]<=m && triplets[i][2]<=r && triplets[i][0]==l)x=i;
+            if(triplets[i][1]==m && triplets[i][2]<=r && triplets[i][0]<=l)y=i;
+            if(triplets[i][1]<=m && triplets[i][2]==r && triplets[i][0]<=l)z=i;
         }
         if(x<0 || y<0 || z<0)return false;
         return true;
