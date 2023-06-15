dea903372ea47fa2c7afde1aa565242a53dd894f
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2023-06-15 14:05:31 +0530

    Time: 33 ms (15.44%), Space: 12.3 MB (51.45%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int dp[305];
+public:
+    map<string,int>m;
+    
+    bool rec(string &s, int i){
+        if(i==s.size())return 1;
+        if(dp[i]!=-1)return dp[i];
+        string temp="";
+        for(int j=i;j<s.size();j++){
+            temp+=s[j];
+            if(m[temp]){
+                if(rec(s,j+1))return dp[i] = 1;
+            }
+        }
+        return dp[i] = 0;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for(string c:wordDict)m[c]++;
+        m[""]++;
+        memset(dp,-1,sizeof(dp));
+        return rec(s,0);
+    }
+};
