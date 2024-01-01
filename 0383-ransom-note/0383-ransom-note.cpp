777abdfa2401779d76fe4992633e43e8ad03697f
Tarun <32845547+Illusion0-0@users.noreply.github.com>
2024-01-01 23:47:22 +0530

    Time: 15 ms (65.57%), Space: 9 MB (97.73%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
+        if(ransomNote.length()>magazine.length())return false;
         int mag[26]{0};
         for(char c:magazine)mag[c-'a']++;
         for(char c:ransomNote)
